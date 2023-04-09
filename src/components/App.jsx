c50cc4c6004ec2fad2c1d074f9c047679d5dae74
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-04-09 12:50:24 +0300

    update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Container } from './Container.styled';
+import { Searchbar } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Toaster } from 'react-hot-toast';
+export class App extends Component {
+  state = {
+    searchText: '',
+    page: 1,
+  };
+
+  createSearchText = searchText => {
+    this.setState({ searchText, page: 1 });
+  };
+
+  handleLoad = () => {
+    this.setState(prev => ({ page: prev.page + 1 }));
+  };
+
+  render() {
+    const searchText = this.state.searchText;
+    return (
+      <Container>
+        <Toaster position="top-right" toastOptions={{ duration: 3000 }} />
+        <Searchbar createSearchText={this.createSearchText} />
+        <ImageGallery
+          createSearch={searchText}
+          searchText={this.state.searchText}
+          page={this.state.page}
+          handleLoad={this.handleLoad}
+        />
+      </Container>
+    );
+  }
+}
