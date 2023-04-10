fc64297b8c7d86793e15db5cc522a3d23e9babb9
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-04-10 15:53:45 +0300

    fix ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -16,7 +16,7 @@ export class ImageGallery extends Component {
     error: null,
   };
 
-  componentDidUpdate(prevProps, prevState) {
+  async componentDidUpdate(prevProps, prevState) {
     const searchText = this.props.searchText.trim();
 
     if (
@@ -24,38 +24,36 @@ export class ImageGallery extends Component {
       prevProps.page !== this.props.page
     ) {
       this.setState({ isLoading: true, error: null });
-      getPixaBay(searchText, this.props.page)
-        .then(data => {
-          const hits = data.hits.length;
-          const totalHits = data.totalHits;
-          const quotientPage = Math.ceil(totalHits / PER_PAGE);
-          this.setState({ cardsToShow: totalHits });
-          if (hits === 0) {
-            this.setState({
-              picture: null,
-            });
-            return toast.error(
-              'Sorry, there are no images matching your search query. Please try again.'
-            );
-          }
-          if (this.props.page !== 1) {
-            this.setState({ picture: [...this.state.picture, ...data.hits] });
-          } else {
-            this.setState({ picture: [...data.hits] });
-          }
-          if (this.props.page === quotientPage) {
-            return toast(
-              "We're sorry, but you've reached the end of search results."
-            );
-          }
-        })
-        .catch(error => {
-          this.setState({ error });
-          toast.error(error.response.data);
-        })
-        .finally(() => {
-          this.setState({ isLoading: false });
-        });
+      try {
+        const data = await getPixaBay(searchText, this.props.page);
+        const hits = data.hits.length;
+        const totalHits = data.totalHits;
+        const quotientPage = Math.ceil(totalHits / PER_PAGE);
+        this.setState({ cardsToShow: totalHits });
+        if (hits === 0) {
+          this.setState({
+            picture: null,
+          });
+          return toast.error(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+        }
+        if (this.props.page !== 1) {
+          this.setState({ picture: [...this.state.picture, ...data.hits] });
+        } else {
+          this.setState({ picture: [...data.hits] });
+        }
+        if (this.props.page === quotientPage) {
+          return toast(
+            "We're sorry, but you've reached the end of search results."
+          );
+        }
+      } catch (error) {
+        this.setState({ error });
+        toast.error(error.response.data);
+      } finally {
+        this.setState({ isLoading: false });
+      }
     }
   }
 
