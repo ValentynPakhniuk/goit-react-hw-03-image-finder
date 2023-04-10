7d6d08acc037b207237a5d3fd3db79d6c98f2099
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-04-10 15:54:14 +0300

    fix Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,19 +1,38 @@
+import { Component } from 'react';
 import { ModalCard, Overlay } from './Modal.styled';
 import PropTypes from 'prop-types';
 
-export const Modal = ({ image }) => {
-  return (
-    <Overlay>
-      <ModalCard>
-        <img src={image.largeImageURL} alt={image.tags} />
-      </ModalCard>
-    </Overlay>
-  );
-};
+export class Modal extends Component {
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown, false);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown, false);
+  }
+
+  handleKeyDown = e => {
+    if (e.keyCode === 27) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    const { image, onClose } = this.props;
+    return (
+      <Overlay onClick={() => onClose}>
+        <ModalCard onClick={e => e.stopPropagation()}>
+          <img src={image.largeImageURL} alt={image.tags} />
+        </ModalCard>
+      </Overlay>
+    );
+  }
+}
 
 Modal.propTypes = {
-  image: PropTypes.exact({
+  image: PropTypes.shape({
     largeImageURL: PropTypes.string.isRequired,
     tags: PropTypes.string.isRequired,
   }).isRequired,
+  onClose: PropTypes.func.isRequired,
 };
